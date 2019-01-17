baf10d88a0d2ffd134e58676a3719d550fa1acf8
Dimitar Tsvetkov <tsvetkovdimitar@gmai.com>
2019-01-17 21:00:27 +0000

    First commit. Main cals created

---
+++ Main.java
@@ -0,0 +1,26 @@
+package snakegame;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+	public Main(){
+		
+		JFrame frame = new JFrame();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Snake");
+		frame.setLocationRelativeTo(null);
+		
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+	
+	public static void main(String[] args){
+		
+		new Main();
+		
+	}
+
+}
---
+++ package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author dimitar
+ *
+ */
+package snakegame;
